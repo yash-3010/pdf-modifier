e059c4bcafaf992954ac0a77053cc9419612773e
yash-kreeti <yash.barman@kreeti.com>
2023-11-09 17:05:02 +0530

    final

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,30 +3,17 @@ import type { NextRequest } from 'next/server'
 import { verifyToken } from './helpers/verifyToken';
 
 export async function middleware(request: NextRequest) {
-    const { pathname } = request.nextUrl;
+    // const { pathname } = request.nextUrl;
 
-    const isPublic = pathname === '/login' || pathname === '/signup';
+    // const isPublic = pathname === '/login' || pathname === '/signup';
 
     // verify token
     const verifiedToken = await verifyToken(request).catch((err) => {
         console.error(err.message)
     })
 
-    if(isPublic){
-        if(verifiedToken){
-            return NextResponse.redirect(new URL('/', request.url));
-        }
-        else{
-            return NextResponse.next();
-        }
-    }
-    else{
-        if(verifiedToken){
-            return NextResponse.next();
-        }
-        else{
-            return NextResponse.redirect(new URL('/login', request.url));
-        }
+    if(!verifiedToken) {
+        return NextResponse.redirect(new URL('/login', request.url));
     }
 
 
@@ -36,8 +23,6 @@ export async function middleware(request: NextRequest) {
 export const config = {
     matcher: [
         '/',
-        '/login',
-        '/signup',
         '/myPdfs(.*)',
         '/downloadPdf(.*)',
         '/modifyPdf(.*)',
