f1e50fa80eb7c2a437863940548fbc3aa84a3f93
yash-kreeti <yash.barman@kreeti.com>
2023-11-10 14:45:20 +0530

    final commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,28 +3,35 @@ import type { NextRequest } from 'next/server'
 import { verifyToken } from './helpers/verifyToken';
 
 export async function middleware(request: NextRequest) {
-    // const { pathname } = request.nextUrl;
+    const { pathname } = request.nextUrl;
 
-    // const isPublic = pathname === '/login' || pathname === '/signup';
+    const isPublic = pathname === '/login' || pathname === '/signup';
+    const isPrivate = pathname === '/myPdfs' || pathname === '/downloadPdf' || pathname === '/modifyPdf' || pathname === '/';
 
     // verify token
     const verifiedToken = await verifyToken(request).catch((err) => {
         console.error(err.message)
     })
 
-    if(!verifiedToken) {
+    if(isPrivate && !verifiedToken) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
+
+    if(isPublic && verifiedToken) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+
 
 
 }
 
 // See "Matching Paths" below to learn more
-export const config = {
-    matcher: [
-        '/',
-        '/myPdfs(.*)',
-        '/downloadPdf(.*)',
-        '/modifyPdf(.*)',
-    ]
-}
+// export const config = {
+//     matcher: [
+//         '/login',
+//         '/signup',
+//         '/myPdfs(.*)',
+//         '/downloadPdf(.*)',
+//         '/modifyPdf(.*)',
+//     ]
+// }
